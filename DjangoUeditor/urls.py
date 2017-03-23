1ff2667e46c534bc398711e8e7dedb16eb1c4607
root <root@HZDE.localdomain>
2017-03-23 14:59:52 +0800

    modifed urls.py to support django 1.9 or above.

--- DjangoUeditor/urls.py
+++ DjangoUeditor/urls.py
@@ -1,12 +1,18 @@
 #coding:utf-8
+# modify to support django 1.9 or above
 from django import VERSION
+from views import get_ueditor_controller
+
 if VERSION[0:2]>(1,3):
     from django.conf.urls import patterns, url
 else:
     from django.conf.urls.defaults import patterns, url
 
-from views import get_ueditor_controller
-
-urlpatterns = patterns('',
-    url(r'^controller/$',get_ueditor_controller)
-)
+if VERSION[0:2]>(1,9):
+	urlpatterns = [
+		url(r'^controller/$',get_ueditor_controller),
+	]
+else:
+	urlpatterns = patterns('',
+		url(r'^controller/$',get_ueditor_controller)
+	)
